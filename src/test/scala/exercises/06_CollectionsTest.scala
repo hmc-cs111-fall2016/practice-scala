2dc8f2ed9b3d815a55a37c1d19876b964f19aaa7
Ben Wiedermann <benw@cs.hmc.edu>
2016-09-22 18:05:22 -0700

    Adding more tests

--- src/test/scala/exercises/06_CollectionsTest.scala
+++ src/test/scala/exercises/06_CollectionsTest.scala
@@ -57,4 +57,32 @@ class CollectionsTest extends FlatSpec {
     assert(List(List(1, 2, 3, 4, 5), List(2, 3, 4, 5), List(3, 4, 5), List(4, 5), List(5), List()) == tails(List(1, 2, 3, 4, 5)))
   }
   
+  "A Scala List" should  "find max int in list" in {
+    assert(9 == maxElementInList(List(1, 9, 4, 9, 8)))
+    assert(25 == maxElementInList(List(1, 7, 5, 17, 25, 24, 22, 19)))
+  }
+
+  it should "calc sum of same positioned elements in two lists" in {
+    assert(List(2, 8, 14) == sumOfTwo(List(1, 5, 9), List(1, 3, 5)))
+    //if one of the lists is empty return the ones with values
+    assert(List(1, 2, 3) == sumOfTwo(List(1, 2, 3), List()))
+    assert(List(1, 2, 3) == sumOfTwo(List(), List(1, 2, 3)))
+  }
+
+  it should "calc sum of same positioned elements in many lists" in {
+    assert(List(12, 15, 18) == sumOfMany(List(1, 2, 3), List(4, 5, 6), List(7, 8, 9)))
+  }
+  
+   it should "rewrite imperative to functional" in {
+    val avery1 = Collections.Person(15, "Avery1", "Jansen")
+    val avery2 = Collections.Person(17, "Avery2", "Janssen")
+    val avery3 = Collections.Person(18, "Avery3", "Jansssen")
+    val jordan1 = Collections.Person(17, "Jordan1", "Peterson")
+    val jordan2 = Collections.Person(19, "Jordan2", "Petersson")
+    val nehal = Collections.Person(21, "Nehal", "Tambe")
+    
+    val result = separateTheYoungFromTheOld(List(nehal, avery1, avery2, avery3, jordan1, jordan2))
+    
+    assert(List(List("Avery1", "Avery2", "Jordan1"), List("Avery3", "Jordan2", "Nehal")) == result)
+  }
 }
